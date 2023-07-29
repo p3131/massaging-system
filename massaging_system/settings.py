99d89fd2706c8b68f1e1409933e843f37dfabdde
p3131 <132905549+p3131@users.noreply.github.com>
2023-07-29 22:45:53 +0300

    Update settings.py

--- massaging_system/settings.py
+++ massaging_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o2z55-s&dzj326p&)tv619)$ganj03=^b1nus1!dz$8^t#r)(=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
